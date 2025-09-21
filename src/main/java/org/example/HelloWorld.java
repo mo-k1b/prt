49a9024b3894f8887758d11f1e1c136d22c353e3
Mohammed Ba Dhib <mohammed.batheeb99@gmail.com>
2025-09-21 15:42:12 +0200

    Initial commit: HelloWorld with two methods and ASCII renderer

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        HelloWorld app = new HelloWorld();
+
+        // Use two methods as required
+        String greeting = app.generateGreeting("World");
+        String farewell = app.generateFarewell("User");
+
+        // Use ASCII Renderer to display messages
+        app.renderMessage(greeting);
+        app.renderMessage(farewell);
+    }
+
+    // Method 1 - will be modified in branchA
+    public String generateGreeting(String name) {
+        return "Hello " + name + "!";
+    }
+
+    // Method 2 - will be modified in branchB
+    public String generateFarewell(String name) {
+        return "Goodbye " + name + "!";
+    }
+
+    // ASCII Renderer method using the external library
+    public void renderMessage(String message) {
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+        builder.width(80).height(10);
+        builder.element(new PseudoText(message));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
+    }
+}
